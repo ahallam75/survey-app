cc33bc910af1715d0a3403f7e97c150731dbb28e
ahallam <alexhallam75@gmail.com>
2019-08-20 09:18:50 -0400

    Login working with header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li key>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav className="container">
@@ -8,15 +28,15 @@ class Header extends React.Component {
           <a href="#" className="left brand-logo">
             Survey-App
           </a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
