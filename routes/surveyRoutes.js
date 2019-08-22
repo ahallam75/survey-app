26b11c79e205526082b81479e27fdc953d970c91
ahallam <alexhallam75@gmail.com>
2019-08-22 11:35:41 -0400

    Still working on mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -16,5 +17,8 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // Send email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
