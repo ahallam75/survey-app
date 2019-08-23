80d1e7f509dec29159bc04baf96c9147caaca842
ahallam <alexhallam75@gmail.com>
2019-08-23 14:15:02 -0400

    SurveyNew.js initial setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as reduxForm } from "redux-form"; // 'as' allow you to rename the variable, i.e. - 'reducer' to 'reduxForm'.
 import authReducer from "./authReducer";
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm
 });
