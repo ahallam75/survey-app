817abdbe586b2d1e26ea351933e07ce706d071f5
ahallam <alexhallam75@gmail.com>
2019-09-04 14:57:51 -0400

    fetch survey action and reducer added

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form"; // 'as' allow you to rename the variable, i.e. - 'reducer' to 'reduxForm'.
 import authReducer from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
